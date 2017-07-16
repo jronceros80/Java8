e8eea0d058fceb38bf100f9dbba7eb2e15c9578f
jronceros80 <jronceros80@gmail.com>
2017-07-16 19:49:16 +0200

    Metodos referenciados

--- PracticasJava8/src/ejercicios/MeRefApp.java
+++ PracticasJava8/src/ejercicios/MeRefApp.java
@@ -1,31 +1,73 @@
 package ejercicios;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class MeRefApp {
 
 	public static void referenciarMetodoStatic(){
 		System.out.println("Metodo referencia static");
 	}
 	
-	public static void referenciarMetodoInstanciaObjetoArbitrario(){
+	public void referenciarMetodoInstanciaObjetoArbitrario(){
+		String[] nombres = { "Mito", "Code", "Jaime" };
 		
+		/*Arrays.sort(nombres, new Comparator<String>() {
+			@Override 
+			public int compare(String o1, String o2) { 
+				return o1.compareToIgnoreCase(o2); 
+			} 
+		});*/
+		 
+		//Arrays.sort(nombres, (s1,s2) -> s1.compareToIgnoreCase(s2));
+		//System.out.println(Arrays.toString(nombres));
+
+		Arrays.sort(nombres, String::compareToIgnoreCase);
+		System.out.println(Arrays.toString(nombres));
 	}
 
 	public static void referenciarMetodoInstanciaObjetoParticular(){
-		
+		System.out.println("Metodo referido de un objeto en particular");
 	}
 
-	public static void referenciarConstructor(){
+	public void referenciarConstructor(){
+		
+		 /*IPersona iper = new IPersona(){
+		  
+		 @Override 
+		 public Persona crear(int id, String nombre) { 
+			 return new Persona(id, nombre); }
+		 }; 
+		 
+		 iper.crear(1, "MitoCode");*/
+
+		 //IPersona iper2 = (x,y)->(new Persona(x,y)); Persona per =
+		 //iper2.crear(1, "MitoCode");
+		 //System.out.println(per.getId()+" - "+per.getNombre());
+		 
+
+		IPersona iper3 = Persona::new;
+		Persona per = iper3.crear(1, "MitoCode");
+		System.out.println(per.getId() + " - " + per.getNombre());
 		
 	}
 	
 	public void operar(){
-		Operacion2 op = ()-> MeRefApp.referenciarMetodoStatic();
-		op.saludar();
+		//Operacion2 op = ()-> MeRefApp.referenciarMetodoStatic();
+		//op.saludar();
+		
+		Operacion2 op2 = MeRefApp::referenciarMetodoStatic;
+		op2.saludar();
 	}
 	
 	public static void main(String[] args) {
 		MeRefApp app = new MeRefApp();
-		app.operar();
+		//app.operar();
+		//app.referenciarMetodoInstanciaObjetoArbitrario();
+		
+		//Operacion2 op2 = MeRefApp::referenciarMetodoInstanciaObjetoParticular;
+		//op2.saludar();
+		app.referenciarConstructor();
 	}
 
 }
